f923737cde1f460640d5b68fe657d36d93123f28
Michael Harris <mwharris333@gmail.com>
2015-02-24 00:59:50 -0500

    added stringifyJSON , passes all tests

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,7 +2,44 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
-
 var stringifyJSON = function(obj) {
-  // your code goes here
+	// your code goes here
+
+	if (obj === null) return 'null';
+	if (typeof obj === 'boolean') return obj.toString();
+	if (typeof obj === 'number') return obj.toString();
+	if (typeof obj === 'string') return '"' + obj + '"';
+
+	if (typeof obj === 'object' && obj.length === 0) return '[]';
+	if (typeof obj === 'object' && obj.length > 0) {
+		var s = '';
+		for(var i=0; i<obj.length; i++){
+			var item = obj[i];
+			if (typeof item !== 'function' && typeof item !== 'symbol' && item !== undefined){
+				s += stringifyJSON(obj[i]) + ',';
+			} else {
+				s += 'null' + ',';				
+			}
+		}
+
+		var isCommaOnEnd = s[s.length-1] === ',';
+		return '['+ (isCommaOnEnd ? s.slice(0,-1):s) +']';	
+	}
+
+	if (typeof obj === 'object' && obj.length === undefined) {
+		var keys = Object.keys(obj);
+		if (keys.length === 0) return '{}';
+		var s = '';
+		for(var i=0; i<keys.length; i++){
+			var item = obj[keys[i]];
+			if (typeof item !== 'function' && typeof item !== 'symbol' && item !== undefined){
+				s += '"'+keys[i]+'":' + stringifyJSON(obj[keys[i]]) + (i < keys.length-1 ? ',':'');
+			}
+		}
+
+		var isCommaOnEnd = s[s.length-1] === ',';
+		return '{'+ (isCommaOnEnd ? s.slice(0,-1):s) +'}';	
+
+	}
+
 };
