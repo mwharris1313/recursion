0641ad0bac3fc82c6730e850552c0b37c5d1a085
Michael Harris <mwharris333@gmail.com>
2015-02-26 03:30:26 -0500

    starting over on parseJSON gonna use parseableStrings in fixtures.js to -
    run tests in a custom main.js program so when a test failure occurs I can
    log it to the console. Might be able to do something similar in Chromes Debug Mode
    with "Watch Expression" but unaware of it.

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,21 +3,5 @@ var pu; // lib/parseUtil.js
 // var parseJSON = JSON.parse;
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-	// your code goes here
-	// var a = parseableStrings[0]; console.log(typeof a, a);
-
-	var s = json.trim();
-	if (s === '[]') return [];
-	if (s === '{}') return {};
-	if (s === 'true') return true;
-	if (s === 'false') return false;
-	if (!isNaN(s)) return +s;
-	if ( pu.isEnclosed(s, '{','}') ) {
-		var sObj = pu.getEnclosed(s, '{', '}');
-		return pu.leftAndRightOf(sObj, ':', true);
-		//return getFirstKey(sObj);
-	}
-
-	return 'nope';
-
+	// your code goes here	
 };
