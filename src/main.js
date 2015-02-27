d59d52bcae12db7021815ae425fc26f970db540c
Michael Harris <mwharris333@gmail.com>
2015-02-27 15:33:30 -0500

    cleanup, replace itemType/itemStarted with isArr/isObj

--- src/main.js
+++ src/main.js
@@ -5,15 +5,16 @@ var out='';
 var runTests = function(){
 	var s = parseableStrings;
 	for(var i=0; i<s.length; i++){
+		log('*********************************',i,s[i]);
+
 		defaultParse = JSON.parse(s[i]);
 		userParse = parseJSON(s[i]);
 		if (!_.isEqual(userParse ,defaultParse)) {
-			log('********************************* FAILED ON');
-			log(i,s[i]);
 			log('--------------------------------- default');
 			log(typeof defaultParse,':',defaultParse);
 			log('--------------------------------- user');
 			log(typeof userParse, ':',userParse);
+			log('********************************* FAILED');
 			return;
 		}
 	}
--- src/parseJSON.js
+++ src/parseJSON.js
@@ -2,68 +2,89 @@ var pu; // lib/parseUtil.js
 // this is what you would do if you were one to do things the easy way:
 // var parseJSON = JSON.parse;
 // but you're not, so you'll write it from scratch:
+var isDebug = false;
 
 var parseJSON = function(json) {
-	// your code goes here
+
 	if (arguments[1] === undefined) { // first run
-		return parseJSON(json, 0, null);
+		return parseJSON(json, 0);
 
 	} else {
 		var s = arguments[0]; // json string
 		var p = arguments[1]; // current position in string
-		var o = arguments[2]; // parent object
+		var o = undefined; // parent object
 
 		var itemStarted = false;
 		var itemEnded = false;
 		var itemType = undefined;
+
 		var keyStart = false;
 		var keyEnd = false;
 		var key = undefined;
+
 		var valStart = false;
 		var valEnd = false;
 		var val = undefined;
 		var valType = undefined;
 
-		for (; p<s.length ;p++) {
+		var isArr = false;
+		var isObj = false;
 
-			if (s[p] === '[' && !itemStarted){ // array start
-				itemStarted = true;
-				itemType = 'array';
+		for (; p<s.length ;p++) { // run through the json string left to right checking for 'trigger' characters
+
+			// ------------------------------------------------------------------ ARRAY INIT
+			if (s[p]==='[' && !isArr && !isObj){ // array start
 				o = [];
-			} else if (s[p] === ']' && itemStarted && itemType === 'array') { // array end
+				valStart = true;
+				isArr = true;
+
+			// ------------------------------------------------------------------ OBJECT INIT
+			} else if (s[p]==='{' && !isArr && !isObj){ // object start
+				o = {};
+				isObj = true;
+
+			// ------------------------------------------------------------------ ARRAY HANDLING
+			} else if (s[p]===']' && isArr) { // array end
 				return o;
 
-			} else if (s[p] === '{' && !itemStarted){ // object start
-				itemStarted = true;
-				itemType = 'object';
-				o = {};
-			} else if (s[p] === '}' && itemStarted && itemType === 'object') { // object end
+//			} else if (s[p] === '"' && itemStarted && itemType === 'array') { // array end
+//				return o;
+
+			// ------------------------------------------------------------------ OBJECT HANDLING
+			} else if (s[p]==='}' && isObj) { // object end
 				if (valEnd) o[key] = val;
 				return o;
 
-			} else if (s[p] === '"' && itemType === 'object' && !keyStart) { // object key start
+			} else if (s[p]==='"' && isObj && !keyStart) { // object key start
 				keyStart = true;
 				key = '';
-			} else if (s[p] !== '"' && keyStart && !keyEnd) { // object key progressing
+			} else if (s[p]!=='"' && isObj && keyStart && !keyEnd) { // object key progressing
 				key += s[p];
 
-			} else if (s[p] === '"' && !keyEnd) { // object key end
+			} else if (s[p]==='"' && isObj && !keyEnd) { // object key end
 				keyEnd = true;
-				log('KEY:',key);
+				if (isDebug) log('KEY:',key);
 
-			} else if (s[p] === ':' && keyEnd && !valStart) { // object val start
+			} else if (s[p]===':' && isObj && keyEnd && !valStart) { // object val start
 				valStart = true;
 
-			} else if (s[p] === '"' && valStart && valType === undefined) { // object val type string
+			// ------------------------------------------------------------------ SHARED HANDLING
+			} else if (s[p]==='"' && (isArr||isObj) && valStart && valType === undefined) { // val type string
 				valType = 'string';
 				val = '';
 
-			} else if (s[p] !== '"' && !valEnd && valType === 'string') { // object val type string progressing
+			} else if (s[p]!=='"' && (isArr||isObj) && !valEnd && valType === 'string') { // val type string progressing
 				val += s[p];
+				if (isDebug) log('VAL PROGRESSING:', val);
 
-			} else if (s[p] === '"' && !valEnd && valType === 'string') { // object val type string end
+			} else if (s[p]==='"' && (isArr||isObj) && !valEnd && valType === 'string') { // val type string end
 				valEnd = true;
-				log('VAL:', val);
+				if (isDebug) log('VAL:', val);
+				if (isArr) o.push(val);
+
+			// if valEnd && s[p]===',' reset all key/val items
+			// ------------------------------------------------------------------ NON HANDLED CHARACTERS
+			// whitespace and other non "trigger" characters...
 
 			} else {
 			}
